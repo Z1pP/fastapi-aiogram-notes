d3fad2dd7fc3652aee4cd475bbc4e75773e3df0a
Z1pP <maxm_99@mail.ru>
2024-09-17 23:23:14 +0300

    add service for update note

--- app/services/note_services.py
+++ app/services/note_services.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from app.exceptions.exceptions import NoteNotFoundException, UserNotFoundException
-from app.schemas.note import NoteCreate, NoteResponse
+from app.schemas import NoteCreate, NoteResponse, NoteUpdate
 from app.models import Tag, Note
 from .user_services import UserService
 
@@ -18,6 +18,16 @@ class NoteService:
         if user:
             return True
         return False
+    
+    async def _add_tags_to_note(self, note: list, tags: list[str]) -> None:
+        for tag_name in tags:
+            tag = await self.session.execute(select(Tag).where(Tag.name == tag_name))
+            tag = tag.scalar_one_or_none()
+            if not tag:
+                tag = Tag(name=tag_name)
+                self.session.add(tag)
+            note.append(tag)
+        return note
 
     async def create_note(self, note: NoteCreate) -> NoteResponse:
         if not await self._check_user_exist(note.user_id):
@@ -25,13 +35,7 @@ class NoteService:
         
         tags_db = []
         if note.tags:
-            for tag_name in note.tags:
-                tag = await self.session.execute(select(Tag).where(Tag.name == tag_name))
-                tag = tag.scalar_one_or_none()
-                if not tag:
-                    tag = Tag(name=tag_name)
-                    self.session.add(tag)
-                tags_db.append(tag)
+            tags_db = await self._add_tags_to_note(tags_db, note.tags)
         
         note_data = note.model_dump(exclude={'tags'})
         note_db = Note(**note_data, tags=tags_db)
@@ -41,6 +45,29 @@ class NoteService:
         await self.session.refresh(note_db)
 
         return note_db
+    
+    async def update_note(self, note_id: int, note: NoteUpdate) -> NoteResponse:
+        query = select(Note).where(Note.id == note_id)
+        result = await self.session.execute(query)
+        db_note = result.scalar_one_or_none()
+
+        if not db_note:
+            raise NoteNotFoundException()
+        
+        note_data = note.model_dump(exclude={'tags'})
+        for key, value in note_data.items():
+            setattr(db_note, key, value)
+        
+        tags_db = []
+        if note.tags:
+            tags_db = await self._add_tags_to_note(tags_db, note.tags)
+            db_note.tags = tags_db
+        
+        await self.session.commit()
+        await self.session.refresh(db_note)
+
+        return db_note
+                
     
     async def get_notes(self) -> list[NoteResponse]:
         result = await self.session.execute(select(Note).options(selectinload(Note.tags)))
