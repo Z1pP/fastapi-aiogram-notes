6b4909177d754c2c78fe4a10ec51ec9e39924cb3
Z1pP <maxm_99@mail.ru>
2024-09-17 23:21:31 +0300

    add update note router

--- app/api/v1/note.py
+++ app/api/v1/note.py
@@ -4,20 +4,20 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_session
 from app.exceptions.exceptions import NoteNotFoundException, UserNotFoundException
-from app.schemas import note as note_schema
+from app.schemas import NoteCreate, NoteResponse, NoteUpdate
 from app.services import NoteService
 
 router = APIRouter(prefix="/notes", tags=["notes"])
 
 
-@router.get("/", response_model=list[note_schema.NoteResponse])
+@router.get("/", response_model=list[NoteResponse])
 async def get_notes(session: AsyncSession=Depends(get_session)):
     note_service = NoteService(session)
     return await note_service.get_notes()
 
 
-@router.post("/create", response_model=note_schema.NoteResponse, status_code=201)
-async def create_note(note: note_schema.NoteCreate, session: AsyncSession=Depends(get_session)):
+@router.post("/create", response_model=NoteResponse, status_code=201)
+async def create_note(note: NoteCreate, session: AsyncSession=Depends(get_session)):
     note_service = NoteService(session)
     try:
         return await note_service.create_note(note)
@@ -32,9 +32,18 @@ async def delete_note(note_id: int, session: AsyncSession=Depends(get_session)):
         return await note_service.delete_note(note_id)
     except NoteNotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
+    
+
+@router.put("/{note_id}", response_model=NoteResponse)
+async def update_note(note_id: int, note: NoteUpdate, session: AsyncSession=Depends(get_session)):
+    note_service = NoteService(session)
+    try:
+        return await note_service.update_note(note_id, note)
+    except NoteNotFoundException as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 
-@router.get("/by_tags", response_model=list[note_schema.NoteResponse])
+@router.get("/by_tags", response_model=list[NoteResponse])
 async def get_notes_by_tags(tags: list[str], session: AsyncSession=Depends(get_session)):
     note_service = NoteService(session)
     return await note_service.get_notes_by_tags(tags)
