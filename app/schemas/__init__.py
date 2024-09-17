8fc855d61836228e3f8d7a3dd5431ba720365408
Z1pP <maxm_99@mail.ru>
2024-09-17 23:22:07 +0300

    add all note schemas in init file

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,9 @@
 from .user import UserResponse, UserCreate
-
+from .note import NoteCreate, NoteResponse, NoteUpdate
 __all__ = [
     "UserResponse",
-    "UserCreate"
+    "UserCreate",
+    "NoteCreate",
+    "NoteResponse",
+    "NoteUpdate"
 ]
