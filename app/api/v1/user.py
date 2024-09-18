880c3426a55717c7eb0120c02e0814908181dcc5
Z1pP <maxm_99@mail.ru>
2024-09-18 21:52:29 +0300

    update import path to user schemas

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -3,21 +3,21 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_async_session
 from app.exceptions.exceptions import UserAlreadyExistsException, UserNotFoundException
-from app.schemas import user as user_schema
+from app.schemas import UserCreate, UserResponse
 from app.services import UserService
 
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.get("/", response_model=list[user_schema.UserResponse])
+@router.get("/", response_model=list[UserResponse])
 async def get_users(session: AsyncSession = Depends(get_async_session)):
     user_service = UserService(session)
     return await user_service.get_users()
 
 
-@router.post('/create', response_model=user_schema.UserResponse)
-async def create_user(user: user_schema.UserCreate, session: AsyncSession = Depends(get_async_session)):
+@router.post('/create', response_model=UserResponse)
+async def create_user(user: UserCreate, session: AsyncSession = Depends(get_async_session)):
     user_seкvice = UserService(session)
     try:
         return await user_seкvice.create_user(user)
@@ -25,7 +25,7 @@ async def create_user(user: user_schema.UserCreate, session: AsyncSession = Depe
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.get("/{user_id}", response_model=user_schema.UserResponse)
+@router.get("/{user_id}", response_model=UserResponse)
 async def get_user_by_id(user_id: int, session: AsyncSession = Depends(get_async_session)):
     user_service = UserService(session)
     try:
