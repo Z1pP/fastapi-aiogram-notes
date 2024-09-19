aa927775a131d910ea25c89d10cd40dbe20c4569
Z1pP <maxm_99@mail.ru>
2024-09-19 20:24:02 +0300

    update note schema

--- app/schemas/note_schema.py
+++ app/schemas/note_schema.py
@@ -8,15 +8,15 @@ class NoteBase(BaseModel):
     title: str = Field(..., min_length=5, max_length=100)
     description: str = Field(..., min_length=1)
     is_completed: bool = Field(default=False)
+    tags: list[TagResponse] = Field(default_factory=list)
 
 
 class NoteCreate(NoteBase):
     user_id: int
-    tags: list[str] = None
 
 
 class NoteUpdate(NoteBase):
-    tags: list[str] = None
+    pass
 
 
 class NoteResponse(NoteBase):
@@ -24,8 +24,7 @@ class NoteResponse(NoteBase):
     user_id: int
     created_at: datetime
     updated_at: datetime
-    tags: list[TagResponse] = Field(default_factory=list)
-
+    
     class Config:
         from_attributes = True
 
