8be6ccd7b4f593b97ce772d3a0874932e673538d
Z1pP <maxm_99@mail.ru>
2024-09-19 20:01:52 +0300

    created tg profile model

---
+++ app/models/tg_profile.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+from datetime import datetime
+from sqlalchemy import DateTime, Integer, String, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
+
+class TgProfile(Base):
+    __tablename__ = "tg_profiles"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    tg_id: Mapped[int] = mapped_column(Integer, unique=True, index=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), name="user_id")
+    username: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=True)
+    first_name: Mapped[str] = mapped_column(String, nullable=True)
+    last_name: Mapped[str] = mapped_column(String, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    user: Mapped["User"] = relationship(uselist=False, back_populates="tg_profile")
