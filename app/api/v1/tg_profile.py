6a7633349796f96484a32e98a5aed3ab0efce242
Z1pP <maxm_99@mail.ru>
2024-09-20 21:26:28 +0300

    add tg profile router

--- app/api/v1/tg_profile.py
+++ app/api/v1/tg_profile.py
@@ -1,4 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.database import get_async_session
+from app.schemas import (
+    TgProfileCreate, 
+    TgProfileResponse, 
+    TgProfileUpdate,
+)
+from app.services import TgProfileService
+from app.exceptions import UserNotFoundException, TgProfileAlreadyExistsException
 
 router = APIRouter(prefix="/tg_profile", tags=["tg_profile"])
+
+
+@router.post("/create", response_model=TgProfileResponse, status_code=201)
+async def create_tg_profile(
+    new_profile: TgProfileCreate, 
+    session: AsyncSession = Depends(get_async_session)
+    ):
+    tg_profile_service = TgProfileService(session)
+    try:
+        return await tg_profile_service.create_tg_profile(new_profile)
+    except UserNotFoundException as e:
+        raise HTTPException(status_code=e.status_code, detail=str(e))
+    except TgProfileAlreadyExistsException as e:
+        raise HTTPException(status_code=e.status_code, detail=str(e))
