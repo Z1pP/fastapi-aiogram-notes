42ef5686584df1c9b87468a17555838f584239ec
Z1pP <maxm_99@mail.ru>
2024-09-20 21:27:36 +0300

    add tg profile schema to init file

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,10 +1,15 @@
 from .user_schema import UserResponse, UserCreate, UserUpdate
 from .note_schema import NoteCreate, NoteResponse, NoteUpdate
+from .tg_profile_schema import TgProfileCreate, TgProfileResponse, TgProfileUpdate
+
 __all__ = [
     "UserResponse",
     "UserCreate",
+    "UserUpdate",
     "NoteCreate",
     "NoteResponse",
     "NoteUpdate",
-    "UserUpdate"
+    TgProfileCreate.__class__.__name__,
+    TgProfileResponse.__class__.__name__,
+    TgProfileUpdate.__class__.__name__,
 ]
