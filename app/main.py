94e312b726be1ed2753b6fa9fc037b28a9e04bf7
Z1pP <maxm_99@mail.ru>
2024-09-20 20:46:44 +0300

    import main api router

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.api.v1.user import router as user_router
-from app.api.v1.note import router as note_router
+from app.api.v1 import router as api_router
 
 app = FastAPI(docs_url="/api", openapi_prefix="/api/v1")
-app.include_router(user_router)
-app.include_router(note_router)
+app.include_router(api_router)
 
 
 if __name__=="__main__":
