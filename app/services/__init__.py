3b1194ddbee87fd3837ef716e1de277247f9fc39
Z1pP <maxm_99@mail.ru>
2024-09-20 21:27:53 +0300

    add tg profile sevice to init file

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,7 +1,9 @@
 from .user_services import UserService
 from .note_services import NoteService
+from .tg_profile_services import TgProfileService
 
 __all__ = [
     "UserService",
     "NoteService",
+    TgProfileService.__class__.__name__,
 ]
