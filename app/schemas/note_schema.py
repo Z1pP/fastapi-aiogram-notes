ed81787cbd2ea4d9bdf1c6df50d813db6db82fdc
Z1pP <maxm_99@mail.ru>
2024-09-22 23:55:18 +0300

    apdate schema

--- app/schemas/note_schema.py
+++ app/schemas/note_schema.py
@@ -1,27 +1,32 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
 
-from .tag_schema import TagResponse
+from .tag_schema import TagResponse, TagCreate
 
 
 class NoteBase(BaseModel):
     title: str = Field(..., min_length=5, max_length=100)
     description: str = Field(..., min_length=1)
     is_completed: bool = Field(default=False)
-    tags: list[TagResponse] = Field(default_factory=list)
+    
 
 
 class NoteCreate(NoteBase):
     user_id: int
+    tags: list[TagCreate]
 
 
-class NoteUpdate(NoteBase):
-    pass
+class NoteUpdate(BaseModel):
+    title: str | None = Field(default=None, min_length=5, max_length=100)
+    description: str | None = Field(default=None, min_length=1)
+    is_completed: bool | None = Field(default=None)
+    tags: list[TagCreate] = Field(default=None)
 
 
 class NoteResponse(NoteBase):
     id: int
     user_id: int
+    tags: list[TagResponse] = Field(default_factory=list)
     created_at: datetime
     updated_at: datetime
     
