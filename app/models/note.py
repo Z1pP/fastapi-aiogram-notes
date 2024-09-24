11b0b10d30236420db10dc1b3908f136cbe1c4c9
Z1pP <maxm_99@mail.ru>
2024-09-24 20:10:02 +0300

    detele Boolean from import

--- app/models/note.py
+++ app/models/note.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
-from sqlalchemy import Boolean, ForeignKey, String, Integer, Text, DateTime
+from sqlalchemy import ForeignKey, String, Integer, Text, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
