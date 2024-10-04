c6d525b7ef559357823b29d194298455d7a17fbc
Z1pP <maxm_99@mail.ru>
2024-10-04 16:43:46 +0300

    refactoring put and delete methods

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, Response, status
 from app.exceptions import *
 from app.schemas import UserCreate, UserResponse, UserUpdate
 from app.services import UserService
-from app.dependencies import get_user_service
+from app.dependencies import get_user_service, get_current_auth_user
 
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -40,26 +40,27 @@ async def get_user_by_id(
         raise HTTPException(status_code=404, detail=str(e))
 
 
-@router.put("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
+@router.put("/", response_model=UserResponse, status_code=status.HTTP_200_OK)
 async def update_user(
-    user_id: int,
     user_updated: UserUpdate,
+    user: UserResponse = Depends(get_current_auth_user),
     user_service: UserService = Depends(get_user_service),
 ):
     try:
-        return await user_service.update_user(user_id, user_updated)
+        return await user_service.update_user(user.id, user_updated)
     except UserNotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
     except UserAlreadyExistsException as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(
-    user_id: int, user_service: UserService = Depends(get_user_service)
+    user: UserResponse = Depends(get_current_auth_user),
+    user_service: UserService = Depends(get_user_service),
 ):
     try:
-        await user_service.delete_user(user_id)
+        await user_service.delete_user(user.id)
         return Response(status_code=204)
     except UserNotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
