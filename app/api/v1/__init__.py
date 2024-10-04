9a38fab60d050a5ef3f7692b292c29b6118975c7
Z1pP <maxm_99@mail.ru>
2024-10-04 15:35:47 +0300

    added auth router to main router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .user import router as user_router
 from .note import router as note_router
 from .tg_profile import router as tg_profile_router
+from .auth import router as auth_router
 
 
 router = APIRouter(prefix="/v1")
 router.include_router(user_router)
 router.include_router(note_router)
 router.include_router(tg_profile_router)
+router.include_router(auth_router)
