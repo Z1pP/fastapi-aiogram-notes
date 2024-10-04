94d1372b287e0db96f342e87c325e4676e48b412
Z1pP <maxm_99@mail.ru>
2024-10-04 15:37:12 +0300

    added

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,6 +2,7 @@ from .user_schema import UserResponse, UserCreate, UserUpdate
 from .note_schema import NoteCreate, NoteResponse, NoteUpdate
 from .tg_profile_schema import TgProfileCreate, TgProfileResponse, TgProfileUpdate
 from .tag_schema import TagCreate, TagResponse
+from .token_schema import TokenInfo
 
 __all__ = [
     "UserResponse",
@@ -15,4 +16,5 @@ __all__ = [
     "TgProfileUpdate",
     "TagCreate",
     "TagResponse",
+    "TokenInfo",
 ]
