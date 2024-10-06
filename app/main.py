31cf3ee971b17bcc2eb8e4362d97c2bf04fc3abc
Z1pP <maxm_99@mail.ru>
2024-10-06 20:20:32 +0300

    fefactoring

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
@@ -7,18 +8,19 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
+from redis.exceptions import ConnectionError as RedisConnectionError
 
 from app.api.v1 import router as api_router
 from app.utils.logger import user_logger, error_logger
-from app.core.config import settings
 
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=[
-        f"{settings.rate_limit.total_request}/{settings.rate_limit.period}"
-    ],
-    storage_uri=f"{settings.rate_limit.redis_url}",
+    default_limits=["100/hour"],
+    storage_uri=f"redis://{REDIS_HOST}:{REDIS_PORT}",
 )
 app = FastAPI(docs_url="/docs", root_path="/api")
 app.state.limiter = limiter
@@ -52,6 +54,22 @@ async def global_exception_handler(request: Request, exc: Exception):
     return JSONResponse(
         status_code=500,
         content={"message": "Внутренняя ошибка сервера. Пожалуйста, попробуйте позже."},
+    )
+
+
+@app.exception_handler(RateLimitExceeded)
+async def custom_rate_limit_exceeded_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429, content={"error": f"Превышен лимит запросов: {exc.detail}"}
+    )
+
+
+@app.exception_handler(RedisConnectionError)
+async def redis_connection_error_handler(request: Request, exc: RedisConnectionError):
+    error_logger.error(f"Ошибка подключения к Redis: {str(exc)}")
+    return JSONResponse(
+        status_code=500,
+        content={"error": "Внутренняя ошибка сервера. Пожалуйста, попробуйте позже."},
     )
 
 
