f3d19a5742036165cb17e6e3e4f4a792ac845a29
Z1pP <maxm_99@mail.ru>
2024-10-08 22:10:45 +0300

    delete unused Boolean import

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
-from sqlalchemy import Boolean, String, Integer, DateTime
+from sqlalchemy import String, Integer, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
