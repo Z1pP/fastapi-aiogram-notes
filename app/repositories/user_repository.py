809ce0c2734d11528778e366e07d2da8b2c1ce61
Z1pP <maxm_99@mail.ru>
2024-10-14 00:01:56 +0300

    remove IUserRepository

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,36 +1,10 @@
-from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from app.models import User
-
-
-class IUserRepository(ABC):
-    @abstractmethod
-    async def get_by_id(self, user_id: int) -> Optional[User]:
-        pass
-
-    @abstractmethod
-    async def get_by_email(self, email: str) -> Optional[User]:
-        pass
-
-    @abstractmethod
-    async def get_all(self) -> List[User]:
-        pass
-
-    @abstractmethod
-    async def add(self, user: User) -> User:
-        pass
-
-    @abstractmethod
-    async def update(self, user: User) -> User:
-        pass
-
-    @abstractmethod
-    async def delete(self, user: User) -> None:
-        pass
+from app.repositories.interfaces import IUserRepository
 
 
 class SQLAlchemyUserRepository(IUserRepository):
