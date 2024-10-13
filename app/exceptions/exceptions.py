8a28aa76270e7ebec9726e93e6cdcdc999306527
Z1pP <maxm_99@mail.ru>
2024-10-13 23:20:01 +0300

    rename AppException to BaseAppEXception

--- app/exceptions/exceptions.py
+++ app/exceptions/exceptions.py
@@ -1,4 +1,4 @@
-class AppException(Exception):
+class BaseAppException(Exception):
     status_code: int = 500  # default status code
 
     def __init__(self, message: str, status_code: int):
@@ -7,31 +7,31 @@ class AppException(Exception):
             self.status_code = status_code
 
 
-class UserAlreadyExistsException(AppException):
+class UserAlreadyExistsException(BaseAppException):
     def __init__(self, email: str):
         self.message = f"User with email {email} already exists"
         super().__init__(self.message, status_code=400)
 
 
-class UserNotFoundException(AppException):
+class UserNotFoundException(BaseAppException):
     def __init__(self):
         self.message = "User not found"
         super().__init__(self.message, status_code=404)
 
 
-class NoteNotFoundException(AppException):
+class NoteNotFoundException(BaseAppException):
     def __init__(self) -> None:
         self.message = "Note not found"
         super().__init__(self.message, status_code=404)
 
 
-class TgProfileAlreadyExistsException(AppException):
+class TgProfileAlreadyExistsException(BaseAppException):
     def __init__(self):
         self.message = "The profile is already linked to another user"
         super().__init__(self.message, status_code=400)
 
 
-class TgProfileNotFoundException(AppException):
+class TgProfileNotFoundException(BaseAppException):
     def __init__(self):
         self.message = "Tg profile not found"
         super().__init__(self.message, status_code=404)
