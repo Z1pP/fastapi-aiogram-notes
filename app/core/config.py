91f37140210bdcc2f4b65366cb210f83d1129ca1
Z1pP <maxm_99@mail.ru>
2024-10-22 21:56:49 +0300

    increment token expire minutes

--- app/core/config.py
+++ app/core/config.py
@@ -9,7 +9,7 @@ class AuthJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
     public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
     algorithm: str = "RS256"
-    access_token_expire_minutes: int = 30
+    access_token_expire_minutes: int = 180
     refresh_token_expire_minutes: int = 60 * 24 * 7  # 1 week
     token_type: str = "Bearer"
 
