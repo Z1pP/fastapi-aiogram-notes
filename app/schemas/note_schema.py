b94f1cd32fa7f2f0273efcbaa8e3113c1153b90c
Z1pP <maxm_99@mail.ru>
2024-10-22 21:53:37 +0300

    added mapping to entity + to response

--- app/schemas/note_schema.py
+++ app/schemas/note_schema.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel, Field
 
-from .tag_schema import TagResponse, TagCreate
+from .tag_schema import TagResponse, TagEntity
 
 
 class NoteBase(BaseModel):
@@ -10,13 +11,27 @@ class NoteBase(BaseModel):
 
 
 class NoteCreate(NoteBase):
-    tags: list[TagCreate]
+    tags: list[str] | None = Field(default=None)
+
+    def to_entity(self) -> "NoteEntity":
+        return NoteEntity(
+            title=self.title,
+            description=self.description,
+            tags=[TagEntity(name=tag) for tag in self.tags],
+        )
 
 
 class NoteUpdate(BaseModel):
     title: str | None = Field(default=None, min_length=5, max_length=100)
     description: str | None = Field(default=None, min_length=1)
-    tags: list[TagCreate] = Field(default=None)
+    tags: list[str] | None = Field(default=None)
+
+    def to_entity(self) -> "NoteEntity":
+        return NoteEntity(
+            title=self.title,
+            description=self.description,
+            tags=[TagEntity(name=tag) for tag in self.tags],
+        )
 
 
 class NoteResponse(NoteBase):
@@ -28,3 +43,19 @@ class NoteResponse(NoteBase):
 
     class Config:
         from_attributes = True
+
+
+class NoteEntity(BaseModel):
+    id: Optional[int] = None
+    user_id: Optional[int] = None
+    title: Optional[str] = None
+    description: Optional[str] = None
+    tags: Optional[list[TagEntity]] = Field(default_factory=list)
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        from_attributes = True
+
+    def to_response(self) -> "NoteResponse":
+        return NoteResponse(**self.model_dump())
