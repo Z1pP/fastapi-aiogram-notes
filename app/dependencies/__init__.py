be50bbbbddc861e642fbc22d2cd9e4fe1073e225
Z1pP <maxm_99@mail.ru>
2024-10-31 22:39:58 +0300

    added depencies for creating user usecases

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -1,4 +1,10 @@
-from app.dependencies.user import get_user_service
+from app.dependencies.user import (
+    get_all_users_use_case,
+    get_user_by_id_use_case,
+    create_user_use_case,
+    update_user_data_use_case,
+    delete_user_use_case,
+)
 from app.dependencies.auth import (
     get_auth_service,
     get_current_auth_user,
@@ -15,15 +21,23 @@ from app.dependencies.note import (
 from app.dependencies.profile import get_tg_profile_service
 
 __all__ = [
-    "get_user_service",
+    # user
+    "get_all_users_use_case",
+    "get_user_by_id_use_case",
+    "create_user_use_case",
+    "update_user_data_use_case",
+    "delete_user_use_case",
+    # auth
     "get_auth_service",
     "get_current_auth_user",
     "get_current_auth_user_refresh",
     "get_current_token_payload",
+    # note
     "get_notes_by_tags_use_case",
     "get_user_notes_use_case",
     "update_note_for_user_use_case",
     "create_note_for_user_use_case",
     "delete_note_for_user_use_case",
+    # profile
     "get_tg_profile_service",
 ]
