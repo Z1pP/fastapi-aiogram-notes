a603485b3bbb540615e7502488b891b15a64b3a3
Z1pP <maxm_99@mail.ru>
2024-10-31 22:39:11 +0300

    user usecases integrated in user router

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,15 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 
 from app.exceptions import BaseAppException
-from app.schemas import UserCreate, UserResponse, UserUpdate, UserEntity
-from app.services import UserService
+from app.schemas import UserCreate, UserResponse, UserUpdate
 from app.dependencies import (
-    get_user_service,
     get_current_auth_user,
     get_all_users_use_case,
     get_user_by_id_use_case,
+    create_user_use_case,
+    update_user_data_use_case,
+    delete_user_use_case,
 )
-from app.usecases.user import GetAllUsersUseCase, GetUserByIdUseCase
+from app.usecases.user import (
+    GetAllUsersUseCase,
+    GetUserByIdUseCase,
+    CreateUserUseCase,
+    UpdateUserDataUseCase,
+    DeleteUserUseCase,
+)
 
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -27,11 +34,10 @@ async def get_users(
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_user(
-    user: UserCreate, user_service: UserService = Depends(get_user_service)
+    user: UserCreate, usecase: CreateUserUseCase = Depends(create_user_use_case)
 ):
     try:
-        created_user = await user_service.create_user(user=user.to_entity())
-        return UserEntity.to_response(created_user)
+        return await usecase.execute(user=user.to_entity())
     except BaseAppException as e:
         raise HTTPException(status_code=e.status_code, detail=str(e))
 
@@ -48,14 +54,12 @@ async def get_user_by_id(
 
 @router.put("/", response_model=UserResponse, status_code=status.HTTP_200_OK)
 async def update_user(
-    user_updated: UserUpdate,
+    user_data: UserUpdate,
     user: UserResponse = Depends(get_current_auth_user),
-    user_service: UserService = Depends(get_user_service),
+    usecase: UpdateUserDataUseCase = Depends(update_user_data_use_case),
 ):
     try:
-        return await user_service.update_user_by_id(
-            user_id=user.id, user=user_updated.to_entity()
-        )
+        return await usecase.execute(user_id=user.id, user=user_data.to_entity())
     except BaseAppException as e:
         raise HTTPException(status_code=e.status_code, detail=str(e))
 
@@ -63,10 +67,10 @@ async def update_user(
 @router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(
     user: UserResponse = Depends(get_current_auth_user),
-    user_service: UserService = Depends(get_user_service),
+    usecase: DeleteUserUseCase = Depends(delete_user_use_case),
 ):
     try:
-        await user_service.delete_user_by_id(user.id)
+        await usecase.execute(user_id=user.id)
         return Response(status_code=204)
     except BaseAppException as e:
         raise HTTPException(status_code=e.status_code, detail=str(e))
