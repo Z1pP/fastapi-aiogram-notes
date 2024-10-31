a1b5f770b364103513d05078659dfd631cd82258
Z1pP <maxm_99@mail.ru>
2024-10-31 22:46:15 +0300

    added depencies for user usecases

--- app/dependencies/user.py
+++ app/dependencies/user.py
@@ -4,7 +4,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.database import get_async_session
 from app.services.user_services import UserService
 from app.repositories import SQLAlchemyUserRepository
-from app.usecases.user import GetAllUsersUseCase, GetUserByIdUseCase
+from app.usecases.user import (
+    GetAllUsersUseCase,
+    GetUserByIdUseCase,
+    UpdateUserDataUseCase,
+    CreateUserUseCase,
+    DeleteUserUseCase,
+)
 
 
 async def get_user_service(
@@ -24,3 +30,21 @@ async def get_user_by_id_use_case(
     user_service: UserService = Depends(get_user_service),
 ) -> GetUserByIdUseCase:
     return GetUserByIdUseCase(user_service=user_service)
+
+
+async def create_user_use_case(
+    user_service: UserService = Depends(get_user_service),
+) -> CreateUserUseCase:
+    return CreateUserUseCase(user_service=user_service)
+
+
+async def update_user_data_use_case(
+    user_service: UserService = Depends(get_user_service),
+) -> UpdateUserDataUseCase:
+    return UpdateUserDataUseCase(user_service=user_service)
+
+
+async def delete_user_use_case(
+    user_service: UserService = Depends(get_user_service),
+) -> DeleteUserUseCase:
+    return DeleteUserUseCase(user_service=user_service)
