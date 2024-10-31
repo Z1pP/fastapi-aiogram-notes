cbe5417427aa06f6c6652deabe1d3f78e21b4910
Z1pP <maxm_99@mail.ru>
2024-10-31 21:48:14 +0300

    added depencies for user usecases

--- app/dependencies/user.py
+++ app/dependencies/user.py
@@ -4,10 +4,23 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.database import get_async_session
 from app.services.user_services import UserService
 from app.repositories import SQLAlchemyUserRepository
+from app.usecases.user import GetAllUsersUseCase, GetUserByIdUseCase
 
 
 async def get_user_service(
     session: AsyncSession = Depends(get_async_session),
 ) -> UserService:
-    user_repository = SQLAlchemyUserRepository(session)
-    return UserService(user_repository)
+    user_repository = SQLAlchemyUserRepository(session=session)
+    return UserService(user_repository=user_repository)
+
+
+async def get_all_users_use_case(
+    user_service: UserService = Depends(get_user_service),
+) -> GetAllUsersUseCase:
+    return GetAllUsersUseCase(user_service=user_service)
+
+
+async def get_user_by_id_use_case(
+    user_service: UserService = Depends(get_user_service),
+) -> GetUserByIdUseCase:
+    return GetUserByIdUseCase(user_service=user_service)
