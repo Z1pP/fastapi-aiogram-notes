62ccd1aa1b5e7356ed61d99f8653ad4075066ba5
Z1pP <maxm_99@mail.ru>
2024-10-31 21:20:21 +0300

    fix imports

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -1,29 +1,29 @@
-from .dependencies import (
-    get_current_token_payload,
-    get_current_auth_user,
-    get_user_service,
-    get_note_service,
+from app.dependencies.user import get_user_service
+from app.dependencies.auth import (
     get_auth_service,
-    get_tg_profile_service,
+    get_current_auth_user,
     get_current_auth_user_refresh,
+    get_current_token_payload,
+)
+from app.dependencies.note import (
+    get_notes_by_tags_use_case,
     get_user_notes_use_case,
+    update_note_for_user_use_case,
     create_note_for_user_use_case,
     delete_note_for_user_use_case,
-    update_note_for_user_use_case,
-    get_notes_by_tags_use_case,
 )
+from app.dependencies.profile import get_tg_profile_service
 
 __all__ = [
-    "get_current_token_payload",
-    "get_current_auth_user",
     "get_user_service",
-    "get_note_service",
     "get_auth_service",
-    "get_tg_profile_service",
+    "get_current_auth_user",
     "get_current_auth_user_refresh",
+    "get_current_token_payload",
+    "get_notes_by_tags_use_case",
     "get_user_notes_use_case",
+    "update_note_for_user_use_case",
     "create_note_for_user_use_case",
     "delete_note_for_user_use_case",
-    "update_note_for_user_use_case",
-    "get_notes_by_tags_use_case",
+    "get_tg_profile_service",
 ]
